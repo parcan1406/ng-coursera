87b7c084d1bdbf5970c32682230870f2e87064c9
andrey <kiselev@intraseti.ru>
2017-10-23 22:08:51 +0300

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import {MaterialModule} from "@angular/material";
 import {FlexLayoutModule} from "@angular/flex-layout";
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by andrey on 23.10.17.
+ */
+export class Dish {
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    description: string;
+}
