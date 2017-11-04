984bc9e2beb3bfa7968c512174415b00f6a0cfcd
andrey <andrey1406@gmail.com>
2017-11-04 19:59:53 +0300

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { FooterComponent } from './footer/footer.component';
 import {AppRoutingModule} from "./app-routing/app-routing.module";
 import {HomeComponent} from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
+import {LeaderService} from "./services/leader.service";
+import {AboutComponent} from "./about/about.component";
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import {ContactComponent} from "./contact/contact.component";
     FooterComponent,
     HomeComponent,
     ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +36,7 @@ import {ContactComponent} from "./contact/contact.component";
       FlexLayoutModule,
       AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
